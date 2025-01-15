d1f795d7d61607d6d01b6c43cb0f9f819c03e127
Jens Navne <jens.navne@gmail.com>
2025-01-15 21:11:33 +0100

    .

--- home-manager/dotfiles.nix
+++ home-manager/dotfiles.nix
@@ -1,11 +1,11 @@
 { pkgs, ... }: {
   home = {
+    file."./.gitconfig".source = ./dotfiles/git/.gitconfig;
     file."./.config/starship.toml".source = ./dotfiles/starship/starship.toml;
     file."./.zsh_aliases".source = ./dotfiles/zsh/.zsh_aliases;
     file."./.zsh_functions".source = ./dotfiles/zsh/.zsh_functions;
     file."./.zshrc".source = ./dotfiles/zsh/.zshrc;
     file."./.warp/themes/norly.yaml".source = ./dotfiles/warp/themes/norly.yaml;
-    file."./.gitconfig".source = ./dotfiles/git/.gitconfig;
   };
 }
 
