0b8f6c7c56d93b6522f440518bfb94037b479cf7
Jens Navne <abjena@almbrand.dk>
2025-01-30 15:08:14 +0100

    added shell.nix

--- home-manager/config/shell.nix
+++ home-manager/config/shell.nix
@@ -1,10 +1,30 @@
-{...}: {
-programs.zsh = {
-    enable = true;
-    ohMyZsh = {
-      enable = true;
-      theme = "euro";
-      plugins = [
+{
+  ...
+}: {
+  programs.zsh  =  {
+    enable  =  true;
+    enableCompletion = true; 
+
+
+    autosuggestion = {
+      enable = true; 
+      strategy = ["completion", "match_prev_cmd", "history"]
+    };
+
+    history = {
+      ignoreDups = true;
+      expireDuplicatesFirst = true; 
+      size = 10000;
+    };
+
+    syntaxHighlighting = {
+      enable = true; 
+    };
+
+    ohMyZsh  =  {
+      enable  =  true;
+      theme  =  "euro";
+      plugins  =  [
         "cp"
         "git"
         "1password"
@@ -12,25 +32,101 @@ programs.zsh = {
       ];
     };
 
-    extraConfig = ''
-      # Disable asynchronous prompt in spaceship
-      SPACESHIP_PROMPT_ASYNC=FALSE
+    localVariables = {
+        # Disable asynchronous prompt in spaceship
+      SPACESHIP_PROMPT_ASYNC = FALSE
+    };
 
-      # Set ZSH options
-      setopt HIST_IGNORE_ALL_DUPS  # Avoid duplicate entries in history
-      setopt AUTO_LIST             # Automatically list options when completing
-      setopt AUTO_MENU             # Cycle through completion options
-      setopt globdots              # Always show hidden files
-
-      # Source external files for aliases and functions
-      ${lib.fileSystems.concatMapStrings (file: "source ${file}") [
-        ./shell_aliases.nix
-        ./shell_functions.nix
-      ]}
-      
+    initExtraFirst  =  ''
+      #TODO: not needed? 
       # Initialize starship prompt
-      eval "$(starship init zsh)"
+      #eval "$(starship init zsh)"
     '';
 
+    shellAliases  =  {
+      ##########################
+      ### Modifying existing ###
+      ##########################
+      sudo = 'sudo ';
+      mkdir = "mkdir -pv";
+      mv = "mv -iv";
+      cp = "cp -iv";
+      du = "ncdu --color dark -rr -x --exclude .git --exclude node_modules";
+      rm = 'trash';
+      ping = "prettyping";
+      #cat = "bat --theme Nord";
+      vim = 'nvim';
+      vi = 'vim';
+      ls = 'lsd -a -l -h --color always --no-symlink --icon always --icon-theme fancy --blocks name,size,date';
+      grep = 'rg --color = auto'; #Ripgrep
+      vscode = 'code';
+      o = 'open';
+
+      ######################
+      ### Custom aliases ###
+      ######################
+      la = 'sudo lsd -lA -h --color always --date relative --icon always --icon-theme fancy --total-size';
+      zshconfig = "vi ~/.zshrc ; echo 'Sourcing zsh file' ; source ~/.zshrc";
+      neofetch = 'clear && neofetch | lolcat';
+      cmatrix = 'cmatrix -C green';
+      dots = "git -C $DOTFILES add -A ; git -C $DOTFILES commit -m 'Changed stuff' ; git -C $DOTFILES push";
+      glog = 'git log --oneline --decorate';
+      reboot = 'sudo reboot';
+      sl = 'sl | lolcat';
+      cya = "dots ; sudo shutdown -h now";
+      up = "darwin-rebuild switch --flake ~/Projects/dots#MacBook-Pro";
+      c = 'code';
+      pullall = "find . -mindepth 1 -maxdepth 1 -type d -exec git --git-dir = {}/.git --work-tree = $PWD/{} pull origin master \;";
+      myip = "curl ipinfo.io/ip";
+      ports = "netstat -t -u -l -a -n";
+      #status = 'watch -c git status';
+      #k = 'kubectl';
+      j = 'autojump';
+      #lazykube = 'k9s';
+      #t = 'terraform';
+      #py = 'python3';
+
+    };
+
+    programs.zsh.initExtra = ''
+    ############################
+    #Make folder and cd into it#
+    ############################
+    function mkcd() {
+        if [[ "$1" ]]; then
+            mkdir "$1" && cd "$1"
+        fi
+    }
+    #############
+    #Git command#
+    #############
+    function skub() {
+        git pull
+        git add -A
+
+        if [ "$1" != "" ]; then
+            git commit -m "$1"
+        else
+            git commit -m "."
+        fi
+        git push
+    }
+
+    ##############
+    #Convert file#
+    ##############
+    function to() {
+        if [ "$1" != "" ]; then
+            if [ "$2" != "" ]; then
+                unoconv -f "$1" "$2"
+                #rmf "$2"
+            else
+                echo "Need to specify file to convert...."
+            fi
+        else
+            echo "Need to specify what to convert to..."
+        fi
+    }
+  '';
   };
 }
