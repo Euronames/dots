f07aa810f906d0c2fd18110f3b9f13d098ce3497
jens.navne@gmail.com <Jens Navne>
2025-01-30 19:25:51 +0100

    .

--- home-manager/dotfiles.nix
+++ home-manager/dotfiles.nix
@@ -6,7 +6,6 @@
     #file."./.zsh_aliases".source = ./dotfiles/zsh/.zsh_aliases;
     #file."./.zsh_functions".source = ./dotfiles/zsh/.zsh_functions;
     #file."./.zshrc".source = ./dotfiles/zsh/.zshrc;
-    file."./.warp/themes/norly.yaml".source = ./dotfiles/warp/themes/norly.yaml;
+    file."./.warp/themes/norly.yaml".source = ./config/themes/warp/norly.yaml;
   };
 }
-
