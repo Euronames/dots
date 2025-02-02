40c72307d1fcddaa58fc7611e9692d7e2188ba04
Jens Navne <jens.navne@gmail.com>
2025-02-02 20:05:15 +0100

    .

--- darwin/services.nix
+++ darwin/services.nix
@@ -6,21 +6,5 @@ args: {
 
     # services.skhd.enable = true;
 
-    #Yabai window manager
-    yabai = {
-      enable = true;
-
-      config = {
-        # focus_follows_mouse = "autoraise";
-        mouse_follows_focus = "off";
-        window_placement = "second_child";
-        window_opacity = "off";
-        top_padding = 36;
-        bottom_padding = 10;
-        left_padding = 10;
-        right_padding = 10;
-        window_gap = 10;
-      };
-    };
   };
 }
