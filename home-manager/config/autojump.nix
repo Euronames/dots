80cd13bd166391d5d445e98452087aa57c084ddf
Jens Navne <jens.navne@gmail.com>
2025-03-23 20:00:13 +0100

    .

--- home-manager/config/autojump.nix
+++
@@ -1,9 +0,0 @@
-args: {
-  programs.autojump = {
-    #TODO: disable, replacing with zoxide
-    enable = false;
-    enableZshIntegration = false;
-    enableBashIntegration = false;
-    enableFishIntegration = false;
-  };
-}
