83fa1276135b55a4b0269c2b0cdb50e1ac372182
Jens Navne <jens.navne@gmail.com>
2025-07-03 10:39:52 +0200

    .

--- darwin/environment.nix
+++ darwin/environment.nix
@@ -9,7 +9,6 @@
     ZSH = "${machineConfig.home}/.oh-my-zsh";
     LOCALBIN = "${machineConfig.home}/.local/bin";
     STARSHIP_CONFIG = "${machineConfig.home}/.config/starship.toml";
-    # Use Nix-managed editors to ensure presence
     EDITOR = "${pkgs.vscode}/bin/code";
     VISUAL = "${pkgs.neovim}/bin/nvim";
 
