54f300d54e0695297e770951e411b330e76f2352
Jens Navne <jens.navne@gmail.com>
2025-07-03 10:48:05 +0200

    Adds GPG key configuration for signing in Git
    Implements GPG key management by introducing a new configuration option for the GPG key, enhancing the portability of the signing setup.
    
    Moves the GPG key definition to the machine configuration for better reusability and maintainability across different environments.
    
    Updates the signing configuration to use the new GPG key reference, ensuring that users have a consistent signing experience.

--- flake.nix
+++ flake.nix
@@ -58,6 +58,7 @@
         homeManager = {
           stateVersion = "23.05";
         };
+        gpgKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDKMrSvVp0l9fKINnGygXRyjoV2kwmZalbWYfOuP/rfHT1t62xL/daZi4w9K/ucS9wQvXkxEaPwiIg7tqfjI4rTjl77WeZVd/sawiRtKBXyTsjjOC81NB0EpgDnv9hU1cggYDOtUE2EwWSjdmAHaG88uR6BxYOhG54A9KOBWYdgreIPzfYhM/CO5ukWs8wVdi9bCmGiADR0h3O0/oxvfDzZiMsU4+blzkjFEOEG0Je9SwbWy3dmRsiL70amny3EDjrMWpv4VmapWpSwwNvau5kX+X0d+KBejmNjE94tegASqAi2ynLbm2A/YhmMzyvCHgME5UVG3fP15dd6QSbFC7T938KtHiT1AOm+pYd04Xn/XqWVY9aa8arL/ChHQnPPXR85JFnTtkSr3w4UowPZrtf7Io+ISXA5TrBONYlcHzZ63d2TraW6Kq71/hfw/OVLnIsEHSHV5Vv2Etm4aM5xWPRhDfoJRoI0dolExpkdq358A2DxkKAIj8PLdw/fErbtRBVFHMw63juhpL3quQKg+qRZ/A6ZtwCR3qkWkhN7QXrKE7ZR0PtwstBBinrwx7VS2Gp4FGrfD87yMiM/DvY4huUKvif+sZL0b6U933OpiRM9EXLQzKNnS3oflAw4sgI/r+syPRGKyV1+1KyT5OK2ldKGMDkDsIcIR3yzzs1/pQsPew==";
       };
       pkgs = import nixpkgs {
         inherit (machineConfig) system;
--- home-manager/config/git.nix
+++ home-manager/config/git.nix
@@ -1,6 +1,7 @@
 {
   lib,
   machineConfig,
+  pkgs,
   ...
 }:
 {
@@ -17,8 +18,9 @@
     inherit (machineConfig) userName userEmail;
 
     signing = {
-      signer = "\${pkgs.gnupg}/bin/gpg2";
-      key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDKMrSvVp0l9fKINnGygXRyjoV2kwmZalbWYfOuP/rfHT1t62xL/daZi4w9K/ucS9wQvXkxEaPwiIg7tqfjI4rTjl77WeZVd/sawiRtKBXyTsjjOC81NB0EpgDnv9hU1cggYDOtUE2EwWSjdmAHaG88uR6BxYOhG54A9KOBWYdgreIPzfYhM/CO5ukWs8wVdi9bCmGiADR0h3O0/oxvfDzZiMsU4+blzkjFEOEG0Je9SwbWy3dmRsiL70amny3EDjrMWpv4VmapWpSwwNvau5kX+X0d+KBejmNjE94tegASqAi2ynLbm2A/YhmMzyvCHgME5UVG3fP15dd6QSbFC7T938KtHiT1AOm+pYd04Xn/XqWVY9aa8arL/ChHQnPPXR85JFnTtkSr3w4UowPZrtf7Io+ISXA5TrBONYlcHzZ63d2TraW6Kq71/hfw/OVLnIsEHSHV5Vv2Etm4aM5xWPRhDfoJRoI0dolExpkdq358A2DxkKAIj8PLdw/fErbtRBVFHMw63juhpL3quQKg+qRZ/A6ZtwCR3qkWkhN7QXrKE7ZR0PtwstBBinrwx7VS2Gp4FGrfD87yMiM/DvY4huUKvif+sZL0b6U933OpiRM9EXLQzKNnS3oflAw4sgI/r+syPRGKyV1+1KyT5OK2ldKGMDkDsIcIR3yzzs1/pQsPew==";
+      signer = "${pkgs.gnupg}/bin/gpg2";
+      # Consider making the GPG key a machineConfig value for portability
+      key = machineConfig.gpgKey;
       signByDefault = true;
     };
 
