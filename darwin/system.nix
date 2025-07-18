731acfdb26a5176ddb0e18975575a6a8b744515f
Jens Navne <jens.navne@gmail.com>
2025-07-18 08:26:09 +0200

    .

--- darwin/system.nix
+++ darwin/system.nix
@@ -3,7 +3,6 @@
   pkgs,
   self,
   machineConfig,
-  inputs,
   ...
 }:
 
@@ -50,30 +49,6 @@
   };
 
   power.sleep.display = 15;
-
-  nix-homebrew = {
-    enable = true;
-
-    user = machineConfig.username;
-    taps = with inputs; {
-      "homebrew/homebrew-core" = homebrew-core;
-      "homebrew/homebrew-bundle" = homebrew-bundle;
-      "homebrew/homebrew-cask" = homebrew-cask;
-    };
-
-    mutableTaps = false; # do not allow installs outside of nix
-    autoMigrate = true; # remember this before first run
-    extraEnv = {
-      HOMEBREW_NO_ANALYTICS = "1";
-    };
-  };
-
-  imports = [
-    ./config.nix
-    ./users.nix
-    ./system.nix
-    ./environment.nix
-  ];
 
   homebrew = import ./homebrew.nix { inherit config; };
 
