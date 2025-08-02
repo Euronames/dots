dd7647fe34cf02364b6007f93e1bf9f86ec308d7
Jens Navne <jens.navne@gmail.com>
2025-08-02 21:19:36 +0200

    .

--- home-manager/config/ssh.nix
+++ home-manager/config/ssh.nix
@@ -7,5 +7,11 @@
     extraConfig = ''
       IdentityAgent = "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
     '';
+    matchBlocks = {
+      proxbox = {
+        hostname = "192.168.0.105";
+        user = "root";
+      };
+    };
   };
 }
