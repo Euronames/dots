076f58b1a18d1b76bbd1f5e5fe4ec0b8a03646cb
Jens <jens.navne@gmail.com>
2025-08-08 11:27:37 +0200

    fix ghostty config path

--- home-manager/dotfiles.nix
+++ home-manager/dotfiles.nix
@@ -1,7 +1,7 @@
 args: {
   home = {
     # file."./.warp/themes/norly.yaml".source = ./config/themes/warp/norly.yaml;
-    file."./.config/ghostty/config".source = ./config/ghostty;
+    file.".config/ghostty/config".source = ./config/ghostty;
     #file.".config/fish/conf.d/nix-env.fish".source = ./config/nix-env.fish;
   };
 }
